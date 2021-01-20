fc91b1114352c28e021d97f83af08ea27b134be3
Michael Scherbela <michael.scherbela@gmail.com>
2021-01-20 23:32:00 +0100

    Added dummy data to DB; improved models; initial draft of profile view

--- app.py
+++ app.py
@@ -1,32 +1,70 @@
-from flask import Flask
+import flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config.from_pyfile('config.py')
 app.config.from_pyfile('instance/config.py')
 db = SQLAlchemy(app)
 admin = Admin(app, name='lunch', template_mode='bootstrap3')
 
 class Restaurant(db.Model):
+    __tablename__ = 'restaurant'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), unique=True, nullable=False)
     url = db.Column(db.String(256), unique=False)
     description = db.Column(db.Text)
     rating = db.Column(db.Float)
+    dishes = db.relationship('Dish', backref='restaurant')
+
+    def __repr__(self):
+        return self.name
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(256))
     last_name = db.Column(db.String(256))
+    dishes = db.relationship('Dish', backref='user')
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Dish(db.Model):
+    __tablename__ = 'dish'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    choices = db.relationship('DishChoice', backref='dish')
+
+    def __repr__(self):
+        return f"{self.name}; {self.restaurant}, {self.user}"
+
+class DishChoice(db.Model):
+    __tablename__ = 'dish_choice'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    dish_id = db.Column(db.Integer, db.ForeignKey('dish.id'), nullable=False)
 
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Restaurant, db.session))
+admin.add_view(ModelView(Dish, db.session))
+admin.add_view(ModelView(DishChoice, db.session))
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/profile/<user_id>')
+def profile(user_id):
+    restaurants = Restaurant.query.all()
+    data = []
+    for r in restaurants:
+        dishes = Dish.query.filter_by(user_id=user_id, restaurant_id=r.id).all()
+        data.append((r.name, [d.name for d in dishes]))
+    return flask.render_template('profile.html', restaurants=data)
 
 if __name__ == '__main__':
     app.run()
--- setupDB.py
+++ setupDB.py
@@ -1,2 +1,22 @@
 from app import db
 db.create_all()
+from app import User, Restaurant, Dish, DishChoice
+
+michael = User(first_name="Michael", last_name="Scherbela")
+pavol = User(first_name="Pavol", last_name="Harar")
+leon = User(first_name="Leon", last_name="Gerard")
+
+oishi = Restaurant(name="Oishi")
+biofrische = Restaurant(name="Bio Frische")
+fladerei = Restaurant(name="Fladerei")
+
+for x in [michael, pavol, leon, oishi, biofrische, fladerei]:
+    db.session.add(x)
+db.session.commit()
+
+db.session.add(Dish(name='Chicken Korma', restaurant_id=biofrische.id, user_id=michael.id))
+db.session.add(Dish(name='Bento Crispy Chicken (mit Minifrühlingsrollen)', restaurant_id=oishi.id, user_id=michael.id))
+db.session.add(Dish(name='Red Curry Tofu', restaurant_id=oishi.id, user_id=pavol.id))
+db.session.add(Dish(name='Red Curry Chicken', restaurant_id=oishi.id, user_id=leon.id))
+db.session.commit()
+
